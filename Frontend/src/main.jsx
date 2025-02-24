bf82fb6e03358de477446d111bd5df456e008e75
niraj mohate <nirajmohate@gmail.com>
2025-02-24 08:16:01 +0530

    change in two files

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import "./index.css";
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client"; // ✅ Use "react-dom/client"
 import App from "./App";
